e5dc28748b6742b4f0bc3e442fcc7c83ca936055
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-04-24 17:56:52 -0300

    Criado  income Api (Add, Get All, Delete, Download Excel)

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,75 @@
+const xlsx = require("xlsx");
+const Income = require("../models/Income");
+
+// Add Income Source
+exports.addIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const { icon, source, amount, date } = req.body;
+    // Validation: Check for missing fields
+    if (!icon || !source || !amount) {
+      res.status(400).json({ message: "all fields are required." });
+    }
+
+    const newIncome = new Income({
+      userId,
+      icon,
+      source,
+      amount,
+      date: date ? new Date(date) : undefined,
+    });
+
+    await newIncome.save();
+    res.status(200).json(newIncome);
+  } catch (error) {
+    res.status(500).json({ message: `Server error: ${error}` });
+  }
+};
+
+// Get All Income Source
+exports.getAllIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+    res.json(income);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error });
+  }
+};
+
+// Delete Income Source
+exports.deleteIncome = async (req, res) => {
+  try {
+    await Income.findByIdAndDelete(req.params.id);
+    res.json({ message: "Income Deleted Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error });
+  }
+};
+
+// Download Excel
+exports.downloadIncomeExcel = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+
+    // Prepare data for Excel
+    const data = income.map((item) => ({
+      Source: item.source,
+      Amount: item.amount,
+      Date: item.date,
+    }));
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+
+    xlsx.utils.book_append_sheet(wb, ws, "Income");
+    xlsx.writeFile(wb, "income_details.xlsx");
+    res.download("income_details.xlsx");
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error });
+  }
+};
